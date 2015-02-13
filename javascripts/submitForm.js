a1ae0da5d976b11f7ee2b03d02eb7cd7556fa17a
Jacob Niebloom <niebloomj@gmail.com>
2015-02-13 16:27:38 -0500

    Parse

--- javascripts/submitForm.js
+++ javascripts/submitForm.js
@@ -1,1 +1,6 @@
 Parse.initialize("DTlgg5qm0fyJxShtYY2RzjX62KgO7jcx4Y3fUV8S", "GYABuByO1Kg8wx8isoHAKPixxzpJLlLtDkx37zhE");
+var TestObject = Parse.Object.extend("TestObject");
+var testObject = new TestObject();
+testObject.save({foo: "bar"}).then(function(object) {
+  alert("yay! it worked");
+});
