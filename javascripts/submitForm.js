8353f97c392e97c110e3e6b3d0409a19074ad85c
PromiscuousFish <jacob.bisnett@gmail.com>
2015-02-13 16:58:11 -0500

    Update submitForm.js

--- javascripts/submitForm.js
+++ javascripts/submitForm.js
@@ -1,11 +1,17 @@
 Parse.initialize("DTlgg5qm0fyJxShtYY2RzjX62KgO7jcx4Y3fUV8S", "GYABuByO1Kg8wx8isoHAKPixxzpJLlLtDkx37zhE");
 var TestObject = Parse.Object.extend("TestObject");
 var testObject = new TestObject();
-var form = document.forms[0]
-var firstName = form.elements[0]
-var lastName = form.elements[1]
-var email = form.elements[2]
+
 
 testObject.save({foo: "bar"}).then(function(object) {
   alert(firstName);
 });
+
+var button = document.getElementById("button");
+button.onclick = function(){
+  var form = document.forms[0]
+  var firstName = form.elements[0]
+  var lastName = form.elements[1]
+  var email = form.elements[2]
+  alert(firstName)
+}
